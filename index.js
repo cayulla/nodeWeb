0236c8edc56846a28545e99621a1d46ad594dda8
Julio Cayulla <jcayulla@gmail.com>
2018-08-19 16:08:41 -0500

    Primera version de la Tienda NodePop

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('html', require('ejs').__express);
+
+app.use(express.static(path.join(__dirname,'images')));
+
+app.get('/',(req,res,next)=>{
+    res.render('login',{title:'Mi Tienda NodePop'});
+});
+
+app.use('/anuncios',require('./routes/anuncios'));
+
+
+app.use((err,req,res,next)=>{
+    res.status(err.status || 500);
+
+    res.locals.message = '';
+    res.locals.error = err;
+
+    res.render('error');
+})
+
+server.listen(3000,()=>{
+    console.log('Iniciado mi Tiende NodePop');
+})
